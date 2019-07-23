8ff0aa7273282ffd5793931197f29308658b905b
Gabe Lyu <no6xichengavenue@gmail.com>
2019-07-23 18:37:28 +0100

    Search module now gives URL only

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from modules import SearchModule
 from selenium.webdriver import Chrome
 
 browser = Chrome()
-search_term = "music artificial intelligence tutorial"
+search_term = "naval seizure tactics"
 search_module = SearchModule(browser=browser, prefix="https://www.google.com/search?q=",
                              block_css="div[class=g]", link_css=".rc .r a",
-                             title_css=".rc .r a h3", next_page_css="td.navend a#pnnext", limit=120,
-                             file_name="music artificial intelligence tutorial.json")
+                             next_page_css="td.navend a#pnnext", limit=120,
+                             file_name="be.txt")
 search_module.search(search_term)
