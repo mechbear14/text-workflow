1381d146a48dddcf26504ea252601323681fceb0
Gabe Lyu <no6xichengavenue@gmail.com>
2019-08-09 08:38:42 +0100

    Updated text module

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 from modules import SearchModule, TextModule
+from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.webdriver import Chrome
+from os import listdir
+from os.path import isfile, join
+import re
 
-# browser = Chrome()
-# search_term = "music artificial intelligence tutorial"
-# search_module = SearchModule(browser=browser, prefix="https://www.google.com/search?q=",
-#                              block_css="div[class=g]", link_css=".rc .r a",
-#                              next_page_css="td.navend a#pnnext", limit=120,
-#                              file_name="links.txt")
-# search_module.search(search_term)
+# TODO: Check Internet connection
+browser = Chrome()
+search_term = "music artificial intelligence tutorial"
+search_module = SearchModule(browser=browser, prefix="https://www.google.com/search?q=",
+                             block_css="div[class=g]", link_css=".rc .r a",
+                             next_page_css="td.navend a#pnnext", limit=120,
+                             file_name="links.txt")
+search_module.search(search_term)
 
 file = open("links.txt")
 
 browser = Chrome()
-for line in file:
-    text_module = TextModule(browser=browser, url=line)
+for index, line in enumerate(file):
+    text_module = TextModule(browser=browser, url=line, index=index)
     text_module.parse_text()
 
 browser.close()
+
+
+# browser = Chrome()
+# browser.get("http://nelatoolkit.science/credibilitytoolkit")
+# clear_btn = browser.find_elements_by_css_selector(".panel-body input[type='button']")[1]
+# clear_btn.click()
+# browser.switch_to.alert.accept()
+# files = [f for f in listdir("./") if isfile(join("./", f)) and re.search(r"20\d{12}-.+\.txt", f)]
+# for file in files:
+#     with open(file) as f:
+#         title = f.readline()
+#         text = ""
+#         line = f.readline()
+#         while line:
+#             text += line
+#             text += "\n"
+#             line = f.readline()
+#         nela = NELAModule(browser, title, text)
+#         try:
+#             nela.assess()
+#         except UnexpectedAlertPresentException:
+#             # browser.switch_to.alert.accept()
+#             with open("nela_problems.txt", "a+") as erf:
+#                 erf.write(f.name)
+#                 erf.write("\n")
+#             continue
+#         except IndexError:
+#             browser.back()
+#             with open("nela_problems.txt", "a+") as erf:
+#                 erf.write(f.name)
+#                 erf.write("\n")
+#             continue
--- modules.py
+++ modules.py
@@ -3,6 +3,7 @@ from urllib.parse import quote
 from datetime import datetime
 from readability import Document
 from bs4 import BeautifulSoup
+import json
 
 
 # TODO: Instead of writing to files, can you return the results directly for the next block to use?
@@ -69,22 +70,44 @@ class SearchModule:
 
 
 class TextModule:
-    def __init__(self, browser, url=None):
+    def __init__(self, browser, url=None, index=0):
         self.browser = browser
         self.url = url
+        self.index = index
 
     def parse_text(self):
         self.browser.get(self.url)
         source = self.browser.execute_script("return Array.from(document.getElementsByTagName('html'))[0].innerHTML")
         doc = Document(source)
-        title = doc.title()
+        title = doc.title().encode("utf-8").decode("ascii", "ignore")
         body = doc.summary()
         soup = BeautifulSoup(body, "html.parser")
-        text = soup.get_text()
-        time_str = datetime.now().strftime("%Y%m%d%H%M%S")
-        title_str = quote(title, safe="")
-        with open("{}-{}.txt".format(time_str, title_str), "w+") as file:
-            file.write(title.encode("utf8").decode("ascii", "ignore"))
-            file.write("\n")
-            file.write(text.encode("utf8").decode("ascii", "ignore"))
+        text = soup.get_text().encode("utf-8").decode("ascii", "ignore")
+        # time_str = datetime.now().strftime("%Y%m%d%H%M%S")
+        # title_str = quote(title, safe="")
+        # with open("{}-{}.txt".format(time_str, title_str), "w+") as file:
+        #     file.write(title.encode("utf8").decode("ascii", "ignore"))
+        #     file.write("\n")
+        #     file.write(text.encode("utf8").decode("ascii", "ignore"))
+        document = dict(title=title, url=self.url, reliability=0, title_obj=0, text_obj=0, text=text)
+        with open("{}.json".format(self.index + 120), "w+") as file:
+            json.dump(obj=document, fp=file)
 
+
+class NELAModule:
+    def __init__(self, browser, title, text):
+        self.browser = browser
+        self.title = title
+        self.text = text
+
+    def assess(self):
+        show_btn = self.browser.find_elements_by_css_selector("input#manual_entry_button")[0]
+        textareas = self.browser.find_elements_by_css_selector("#manual_entry textarea")
+        title_box = textareas[0]
+        text_box = textareas[1]
+        submit_btn = self.browser.find_elements_by_css_selector("#manual_entry input[type='submit']")[0]
+
+        show_btn.click()
+        title_box.send_keys(self.title)
+        text_box.send_keys(self.text)
+        submit_btn.click()
